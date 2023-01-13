a8326e9ea08bda707f2875d6b30e3464a3de7ed5
DevvSakib <devvsakib@gmail.com>
2023-01-13 14:46:51 +0600

    literals in java

---
+++ Day2/Literal.java
@@ -0,0 +1,13 @@
+public class Literal {
+    public static void main(String[] args) {
+        int number = 12;
+        float flt = 12.4f; // f or f
+        double dbl = 122.434d; // d or D
+        String str = "Hello, duble quote";
+        char str2 = 'H'; // only one character
+        byte age = 90; // range -128 to 127
+        long lng = 343243432423L; // add L or l for long data type
+        boolean bl = false; // true false
+        System.out.println(bl);
+    }
+}
