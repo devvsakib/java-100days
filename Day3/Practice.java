4b10a9abe08da6d4be3fd64eecb2817385137ced
DevvSakib <devvsakib@gmail.com>
2023-01-14 22:25:21 +0600

    Day3 done with practice

---
+++ Day3/Practice.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args){
+        // Excercise 1 -> sum of three
+        // int a = 10;
+        // int b = 20;
+        // int c = 30;
+        // int sum = a + b + c;
+        // System.out.println(sum);
+
+        // Excercise 2 -> average number
+        // float eng = 55;
+        // float math = 85;
+        // float ict = 90;
+        // float cgpa = (eng + math + ict) / 30;
+        // System.out.println(cgpa);
+
+        // Excercise 3 -> get user input 
+        // System.out.println("Your name:");
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.next(); 
+        // System.out.println("Hello" + name);
+
+        // Excercise 4 -> check if it's integer
+        // System.out.println("Number:");
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println(sc.hasNextInt());
+
+    }    
+}
