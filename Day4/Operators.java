530a285f38bd3280361a416782cc3913afaf6846
DevvSakib <devvsakib@gmail.com>
2023-01-15 21:24:08 +0600

    Day4 Operators in Java

---
+++ Day4/Operators.java
@@ -0,0 +1,39 @@
+public class OperatorsJava {
+    public static void main(String[] args) {
+        // System.out.println("Hello Day4!");
+        int a = 1;
+        // int b = 2 + a;
+        // int b = 2 * a;
+        // int b = 16 % 3; // modulo operator
+        int b = 16 / 3;
+        a += b;
+        // System.out.println(b);
+        // System.out.println(b == a);
+        // System.out.println(b != a);
+        // System.out.println(b > a);
+        // Logical
+        // System.out.println((2>4 && 4 > 1) || 5<10);
+    
+        //  || or- one true/false
+        
+        // Bitwuse
+        int x = 10;
+        // System.out.println(x&3);
+
+        System.out.println(4.52%1);
+    
+    
+    }
+}
+
+// Operators are used to perform operations on variables and values
+
+// Type of
+// Arithmetic -> +, -, /, *, ++, --
+// Assignment -> =, +=, -=
+// Comparison -> ==, >=, <=
+// Logical -> &&, ||, !
+// Bitwise -> &, |
+
+// Arithmetic operators cnnt work with booleans
+// % can work with float, double
