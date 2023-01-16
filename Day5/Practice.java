9fcd0edbf8cec50212e1b15cdcff8922eb50c826
DevvSakib <devvsakib@gmail.com>
2023-01-16 23:11:02 +0600

    Day5 ResultDataType & Practice

---
+++ Day5/Practice.java
@@ -0,0 +1,19 @@
+import java.u3til.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        // int v, u, a, s;
+        // v = 5;
+        // u = 2;
+        // a = 1;
+        // s = 5;
+        // int x = (v * v - u * u) / (2 * a * s);
+        // System.out.println(x);
+        // char grade = 'B';
+        // grade = (char) (grade - 8);
+        // System.out.println(grade);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a > 8);
+    }
+}
---
+++ Day5/ResultingDataType.java
@@ -0,0 +1,41 @@
+public class ResultingDataType {
+    public static void main(String[] args) {
+        // flaot + int = float
+        // float t = 654.5f + 6;
+        // byte x = 5;
+        // int y = 5;
+        // short z = 3;
+        // int a = y + z;
+        // System.out.println(a);
+        // Increment and Decrement Operators
+        // int i = 54;
+        // int p = i++;
+
+        // use i first, it no matter to what
+        // System.out.println(p); // 54
+        // System.out.println(i++);
+        // i increased by 1
+        // System.out.println(i);
+        // first increase i then print
+        // System.out.println(++i);
+        // i increased to 56
+        // System.out.println(i);
+        // System.out.println(i++);
+        // System.out.println(i);
+        // System.out.println(--i);
+
+        // char
+        char a = 'A';
+        System.out.println(a++); // A
+        System.out.println(++a); // C
+    }
+}
+
+// byte + short ---> int
+// int + short ---> int
+// long + float ---> float
+// int + float ---> float
+// char + int ---> int
+// char + short ---> int
+// long + double ---> double
+// flaot + double ---> double
