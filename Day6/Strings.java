237b68d977a8188b74c337d5b5c5e5a005f633d6
DevvSakib <devvsakib@gmail.com>
2023-01-17 23:19:07 +0600

    String in Java

---
+++ Day6/Strings.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+        String name = new String("Hello DevvSakib");
+        // System.out.println(name);
+        int a = 5;
+        float b = 5.524f;
+
+        // System.out.printf("the value %d of a: %10.2f", a, b);
+        // %10.2f -> 10 = 10 space, .2 -> first 2 decimal
+        // System.out.format("the value %d of a: %f", a, b);
+        // Scanner sc = new Scanner(System.in);
+        // String st = sc.next();
+        // String st = sc.nextLine();
+        // System.out.println(st);
+
+        /*
+         * next() => take first word 
+         * nextLine() => take all word 
+         * 
+         * 
+         * printf or format
+         * 
+         * 
+         * %d --> format spacifier
+         * %d -> int
+         * %f -> float
+         * %c -> char
+         * %s -> string
+         */
+
+    }
+}
+/*
+ * A string is a sequence of characters.
+ * String name = new String("DevvSakib")
+ * 
+ * String is a class but can be use like a data type
+ * 
+ * string are immutable, cant be change
+ * 
+ * 
+ */
