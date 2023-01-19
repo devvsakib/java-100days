b7a28b458799d7f614e8f8b7ffbb91761d2c514a
DevvSakib <devvsakib@gmail.com>
2023-01-19 23:20:54 +0600

    if else condition

---
+++ Day8/ControlFlow.java
@@ -0,0 +1,11 @@
+public class ControlFlow {
+    public static void main(String[] args) {
+        int age = 28;
+        if (age >= 18) {
+            System.out.println("You passed!");
+        }
+        else{
+            System.out.println("Not accepted.");
+        }
+    }
+}
