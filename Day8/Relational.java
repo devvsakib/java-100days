34566357e82c64cf1e055e78d6473064f2e55caa
DevvSakib <devvsakib@gmail.com>
2023-01-19 23:40:15 +0600

    Day8 Control Flow

--- Day8/Relational.java
+++ Day8/Relational.java
@@ -1,26 +1,28 @@
 public class Relational {
     public static void main(String[] args) {
         // if (2 > 2 && 2 < 3) {
-        //     System.out.println(true);
+        // System.out.println(true);
         // }else{
-        //     System.out.println(false);
+        // System.out.println(false);
         // }
 
         // if (2 > 5 || 2 < 3) {
-        //     System.out.println(true);
+        // System.out.println(true);
         // }else{
-        //     System.out.println(false);
+        // System.out.println(false);
         // }
-        
+
         // if (2 > 5 != 2 < 3) {
-        //     System.out.println(true);
+        // System.out.println(true);
         // }else{
-        //     System.out.println(false);
+        // System.out.println(false);
         // }
 
         if (!(2 > 5)) {
             System.out.println(true);
-        }else{
+        } else if ((5 > 2)) {
+            System.out.println(true);
+        } else {
             System.out.println(false);
         }
 
--- Day8/SwitchJava.java
+++ Day8/SwitchJava.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;
+
 public class SwitchJava {
-    
+    public static void main(String[] args) {
+        // int age = 40;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        // if (age > 49) {
+        // System.out.println("I'm in half century");
+        // } else {
+        // System.out.println("I'm not half century yet");
+        // }
+        switch (age) {
+            case 30:
+                System.out.println("Age under 30");
+                break;
+            case 35:
+                System.out.println("Age under 35");
+                break;
+            case 50:
+                System.out.println("I'm in  half century");
+                break;
+            case 51:
+                System.out.println("I'm in  half century");
+                break;
+            case 52:
+                System.out.println("I'm in  half century");
+                break;
+            default:
+                System.out.println("Enjoy");
+                break;
+        }
+    }
 }
+//  breack -> kill flow  
