fa0122b649df8be5f8145ba383bc0f2e9c9f1a51
DevvSakib <devvsakib@gmail.com>
2023-01-19 23:31:44 +0600

    relational and conditinal

---
+++ Day8/Relational.java
@@ -0,0 +1,43 @@
+public class Relational {
+    public static void main(String[] args) {
+        // if (2 > 2 && 2 < 3) {
+        //     System.out.println(true);
+        // }else{
+        //     System.out.println(false);
+        // }
+
+        // if (2 > 5 || 2 < 3) {
+        //     System.out.println(true);
+        // }else{
+        //     System.out.println(false);
+        // }
+        
+        // if (2 > 5 != 2 < 3) {
+        //     System.out.println(true);
+        // }else{
+        //     System.out.println(false);
+        // }
+
+        if (!(2 > 5)) {
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+
+    }
+}
+
+/*
+ * Relational Operators
+ * == -> equal
+ * != -> not equal
+ * > -> greater
+ * < -> less
+ * <= -> eq g
+ * >= -> eq l
+ * && -> and (most commonly used)
+ * || -> or (most commonly used)
+ * ! -> not (most commonly used)
+ * 
+ * 
+ */
