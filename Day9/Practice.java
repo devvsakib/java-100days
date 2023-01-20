c84fca4492fee98d3b50441d255bda4bb5dec003
DevvSakib <devvsakib@gmail.com>
2023-01-20 23:51:45 +0600

    Day9 Practice

---
+++ Day9/Practice.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        // Practice 1
+        // int a = 10;
+        // if (a == 11) {
+        // System.out.println("Equal");
+        // }else{
+        // System.out.println("Not Equal");
+        // }
+
+        // Practice 2
+        // byte m1, m2, m3;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter sub mark 1: ");
+        // m1 = sc.nextByte();
+        // System.out.println("Enter sub mark 2: ");
+        // m2 = sc.nextByte();
+        // System.out.println("Enter sub mark 3: ");
+        // m3 = sc.nextByte();
+
+        // float avg = (m1 + m2 + m3) / 3f;
+        // if (avg >= 40 && m1 >= 33 && m2 >= 33 && m3 >= 33) {
+        // System.out.println("Passed");
+        // } else {
+        // System.out.println("You failed");
+        // }
+
+        // Practice 3
+        /*
+         * Scanner sc = new Scanner(System.in);
+         * float tax = 0;
+         * float income = sc.nextFloat();
+         * if (income < 2.5f) {
+         * tax = income + tax;
+         * }
+         * if (income > 2.5f && income <= 5.0f) {
+         * tax = tax + 0.05f * (income - 2.5f);
+         * } else if (income > 5.0f && income <= 10.0f) {
+         * tax = tax + 0.05f * (income - 2.5f);
+         * tax = tax + 0.2f * (income - 5f);
+         * } else if (income > 10.0f) {
+         * tax = tax + 0.05f * (income - 2.5f);
+         * tax = tax + 0.2f * (income - 5f);
+         * tax = tax + 0.3f * (income - 10.0f);
+         * }
+         * System.out.println("Total tax is:" + tax);
+         */
+        /*
+         * Practice 4
+         * Scanner sc = new Scanner(System.in);
+         * int day = sc.nextInt(1);
+         * switch (day) {
+         * // enhance switch
+         * case 1 -> System.out.println("Friday");
+         * case 2 -> System.out.println("Saterday");
+         * case 3 -> System.out.println("Sunday");
+         * case 4 -> System.out.println("Monday");
+         * case 5 -> System.out.println("Tuesday");
+         * case 6 -> System.out.println("Wednesday");
+         * case 7 -> System.out.println("Thusday");
+         * }
+         */
+            
+
+    }
+}
