8a25a201b440bfcfc9a02537d73d2f5a963f85fd
DevvSakib <devvsakib@gmail.com>
2023-01-21 23:51:13 +0600

    break in loop

---
+++ Day10/BreakContinue.java
@@ -0,0 +1,26 @@
+public class BreakContinue {
+    public static void main(String[] args) {
+
+        // for (int j = 0; j < 10; j++) {
+        // System.out.println(j);
+
+        // // end loop when j equal 3
+        // if (j == 3) {
+        // System.out.println("End Loop");
+        // break;
+        // }
+        // }
+
+        // int i = 3;
+        // while (i < 10) {
+        //     if (i == 7) {
+        //         System.out.println("End Loop");
+        //         break;
+        //     }
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        
+    }
+}
