9fd994aa7b3b578f17ef1ebc570f526a03548728
DevvSakib <devvsakib@gmail.com>
2023-01-21 23:32:37 +0600

    while loop

---
+++ Day10/LoopInJava.java
@@ -0,0 +1,27 @@
+public class LoopInJava {
+    public static void main(String[] args) {
+        int i = 0;
+
+        // While Loop
+        // while (i < 5) {
+        // System.out.println(i);
+        // i++;
+        // }
+        // while (true) {
+        // System.out.println(i);
+        // i++;
+        // }
+        
+
+    }
+}
+/*
+ * LOOP TYPE
+ * ---------
+ * Primary thre type
+ * -> While Loop
+ * -> Do-While Loop
+ * -> For Loop
+ * 
+ * No description needed as we all already know how they works.
+ */
