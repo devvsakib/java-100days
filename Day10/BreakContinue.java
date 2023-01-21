883b9a96053bbda881eaa738f90ac02b43304542
DevvSakib <devvsakib@gmail.com>
2023-01-21 23:56:58 +0600

    Day10 loop and break continue

--- Day10/BreakContinue.java
+++ Day10/BreakContinue.java
@@ -13,14 +13,32 @@ public class BreakContinue {
 
         // int i = 3;
         // while (i < 10) {
-        //     if (i == 7) {
-        //         System.out.println("End Loop");
-        //         break;
-        //     }
-        //     System.out.println(i);
-        //     i++;
+        // if (i == 7) {
+        // System.out.println("End Loop");
+        // break;
+        // }
+        // System.out.println(i);
+        // i++;
         // }
 
-        
+        // Continue
+        // for (int j = 0; j < 10; j++) {
+        // if (j == 7) {
+        // System.out.println("Ignore below code");
+        // continue;
+        // }
+        // System.out.println(j);
+        // }
+
+        int i = 2;
+        do {
+            i++;
+            if (i == 4) {
+                System.out.println("Ignore 4");
+                continue;
+            }
+            System.out.println(i);
+        }while (i < 10);
+
     }
 }
