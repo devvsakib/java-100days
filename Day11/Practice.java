a6bc85a0fec00b47935559c56059296492dff3ff
DevvSakib <devvsakib@gmail.com>
2023-01-22 23:26:27 +0600

    p1

---
+++ Day11/Practice.java
@@ -0,0 +1,16 @@
+public class Practice {
+    public static void main(String[] args) {
+
+        // practice 1
+        // int n = 4;
+        // for (int i = n; i > 0; i--) {
+        // for (int j = 0; j < i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println("\n");
+        // }
+
+
+
+    }
+}
