4bf5d73c91617e5dc512f7a1a7376c1a568b59f2
DevvSakib <devvsakib@gmail.com>
2023-01-24 23:16:34 +0600

    practice 1 2 3 done

---
+++ Day13/Practice.java
@@ -0,0 +1,38 @@
+public class Practice {
+    public static void main(String[] args) {
+
+        // Practice 1
+        // float[] marks = {50.45f, 76.4f,87.56f, 100.0f};
+        // float sum = 0;
+        // for (float f : marks) {
+        // sum += f;
+        // }
+        // System.out.println(sum);
+
+        // Practice 2
+        // float[] marks = {50.45f, 76.4f,87.56f, 100.0f};
+        // float num = 87.556f;
+        // float num = 87.556f;
+        // boolean isArray = false
+        
+        // Practice 3
+        // float[] marks = {50.45f, 76.4f,87.56f, 100.0f};
+        // float sum = 0;
+        // for (float f : marks) {
+        // sum += f;
+        // }
+        // sum = sum / marks.length;
+        // System.out.println(sum);
+        
+
+
+
+
+
+
+
+
+
+
+    }
+}
