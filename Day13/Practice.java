d68167e0965e2310185b50245a8b97145824ca77
DevvSakib <devvsakib@gmail.com>
2023-01-24 23:54:03 +0600

    swap array, reverse array

--- Day13/Practice.java
+++ Day13/Practice.java
@@ -25,24 +25,39 @@ public class Practice {
         // System.out.println(sum);
 
         // Practice 4 addition of two 2D array
-        int[][] mat1 = {
-                { 1, 2, 3 },
-                { 4, 5, 6 } };
-        int[][] mat2 = {
-                { 3, 4, 5 },
-                { 6, 5, 8 } };
-        int[][] result = {
-                { 0, 0, 0 },
-                { 0, 0, 0 } };
+        /*
+         * int[][] mat1 = {
+         * { 1, 2, 3 },
+         * { 4, 5, 6 } };
+         * int[][] mat2 = {
+         * { 3, 4, 5 },
+         * { 6, 5, 8 } };
+         * int[][] result = {
+         * { 0, 0, 0 },
+         * { 0, 0, 0 } };
+         * 
+         * for (int m1 = 0; m1 < mat1.length; m1++) { // row
+         * for (int i = 0; i < mat1[m1].length; i++) { // column
+         * result[m1][i] = mat1[m1][i] + mat2[m1][i];
+         * System.out.print(result[m1][i] + " ");
+         * }
+         * System.out.println();
+         * }
+         */
 
-        for (int m1 = 0; m1 < mat1.length; m1++) {  // row
-            for (int i = 0; i < mat1[m1].length; i++) { // column
-                result[m1][i] = mat1[m1][i] + mat2[m1][i];
-                System.out.print(result[m1][i] + " ");
-            }
-            System.out.println();
+        // Practice 4 Reverse array
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        int a = Math.floorDiv(arr.length, 2);
+        int temp;
+        for (int i = 0; i < a; i++) {
+            // swap a[i] [l-1-i]
+            temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
         }
-
+        for (int e : arr) {
+            System.out.println(e);
+        }
 
     }
 }
