c6301adeadfe2cad76f852907ea89ced3fbf41dd
DevvSakib <devvsakib@gmail.com>
2023-01-25 23:51:05 +0600

    Day13 swap array and more

--- Day13/Practice.java
+++ Day13/Practice.java
@@ -46,7 +46,7 @@ public class Practice {
          */
 
         // Practice 4 Reverse array
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        int[] arr = { 1, 65, 78, 10, -5, 6, 7 };
         int a = Math.floorDiv(arr.length, 2);
         int temp;
         for (int i = 0; i < a; i++) {
@@ -58,6 +58,25 @@ public class Practice {
         for (int e : arr) {
             System.out.println(e);
         }
+        // Practice 5
+        int max = Integer.MIN_VALUE;
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        System.out.println(max);
+        // System.out.println(Integer.MAX_VALUE);
+
+        // Practice 6
+        boolean isSorted = true;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+        System.out.println(isSorted);
 
     }
 }
