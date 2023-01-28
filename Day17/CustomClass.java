e75054136b36692d545ed5f3dc72e09adb9c27e1
DevvSakib <devvsakib@gmail.com>
2023-01-28 22:07:57 +0600

    Day17 OOP, Custom Class

---
+++ Day17/CustomClass.java
@@ -0,0 +1,43 @@
+class Employee {
+    int id;
+    String name;
+    public void printDetails(int a){
+        System.out.println("Custom Code: " + a);
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+    }
+}
+
+public class CustomClass {
+
+    public static void main(String[] args) {
+
+        // intansiating new Employee Obj
+        Employee sakib = new Employee(); 
+        Employee tobi = new Employee();
+        
+        // setting attributes
+        sakib.id = 12;
+        sakib.name = "DevvSakib";
+        
+        tobi.id = 13;
+        tobi.name = "Tobiloba";
+        
+        // printing attributes
+        // sakib.printDetails();
+        // System.out.println();
+        // tobi.printDetails();
+        sakib.printDetails(1);
+        System.out.println();
+        tobi.printDetails(2);
+
+    }
+}
+/*
+ * [One Java file one public class]
+ * 
+ * We can write custom class
+ * 
+ * 
+ * 
+ */
