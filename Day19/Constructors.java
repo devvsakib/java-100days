ac254de4c0b3aba26da9b4844ade1fb88edd47d9
DevvSakib <devvsakib@gmail.com>
2023-01-30 21:10:51 +0600

    contructors in java

--- Day19/Constructors.java
+++ Day19/Constructors.java
@@ -1,6 +1,50 @@
+import javax.swing.border.EmptyBorder;
+
+class myEmployee {
+    private int id;
+    private String name;
+
+    // Constructors
+    public myEmployee(int myid, String myname) {
+        id = myid;
+        name = myname;
+    }
+
+    // Constructor overload
+    public myEmployee() {
+        id = 0;
+        name = "Demo";
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setId(int x) {
+        id = x;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
 public class Constructors {
 
     public static void main(String[] args) {
-        
+        myEmployee ds = new myEmployee();
+        System.out.println(ds.getId());
+        System.out.println(ds.getName());
+
     }
 }
+
+/*
+ * Contructore name should be same as class name.
+ * 
+ * 
+ */
