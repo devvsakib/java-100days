07b5f83bca89771f913c035b8f87c3a6a86d83f7
DevvSakib <devvsakib@gmail.com>
2023-02-06 12:07:26 +0600

    Day25 Interfaces java

--- Day25/Interfaces.java
+++ Day25/Interfaces.java
@@ -1,31 +1,56 @@
-import java.net.SocketException;
-
 /**
  * InnerInterfaces
  **/
 interface Bicycle {
+    int a = 50;
+
+    // final int a = 50; //final is not require to write, its default
     void applyBrack(int decSpeed);
 
     void speedUp(int incSpeed);
 }
 
-class ElectricBicycle implements Bicycle {
+interface HornBicycle {
+    void blowHornK3g();
+
+    void blowHornMnh();
+}
+
+class ElectricBicycle implements Bicycle, HornBicycle {
     int speed = 10;
+
+    void pressHorn() {
+        System.out.println("Pep Pep...");
+    }
 
     public void applyBrack(int decSpeed) {
         speed = speed - decSpeed;
+        System.out.println("Brake pressed...");
     };
 
     public void speedUp(int incSpeed) {
         speed += incSpeed;
+        System.out.println("Speeding up cycle...");
+    }
+    public void blowHornK3g(){
+        System.out.println("Horn k3G from another interface");
+    }
+    public void blowHornMnh(){
+        System.out.println("Horn Mnh from another interface");
     }
 
 }
 
 public class Interfaces {
     public static void main(String[] args) {
-        Bicycle bc = new ElectricBicycle();
- 
+        ElectricBicycle bc = new ElectricBicycle();
+        bc.applyBrack(10);
+        bc.pressHorn();
+        bc.speedUp(1);
+
+        // cannot modify the properties in Interface, they are final
+        bc.blowHornK3g();
+        bc.blowHornMnh();
     }
 }
 /*
