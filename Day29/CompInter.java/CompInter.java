41f3cc26feeaff83f46b41b72b17f9414ba374d7
DevvSakib <devvsakib@gmail.com>
2023-02-10 23:28:53 +0600

    interpreter vs compiler

---
+++ Day29/CompInter.java/CompInter.java
@@ -0,0 +1,20 @@
+public class CompInter {
+
+}
+
+/*
+ * 
+ * Interpreter translate one statement at a time into machine code.
+ * One statement at a time
+ * Interpreter needed everytime
+ * Partial execution error
+ * Easy for programmers
+ * 
+ * 
+ * Compiler scan whole program and turn into machine code.
+ * Entire program at a time
+ * Once compile
+ * No execution for error
+ * Not easy as interpreter
+ * 
+ */
