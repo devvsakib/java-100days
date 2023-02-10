f66c53003c456ec7ccf20d22a4862cc50e5da982
DevvSakib <devvsakib@gmail.com>
2023-02-10 23:40:27 +0600

    hybrid java

--- Day29/CompInter.java/CompInter.java
+++ Day29/CompInter.java/CompInter.java
@@ -1,8 +1,24 @@
 public class CompInter {
-
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
 }
 
 /*
+ * Hybrid Language Java
+ * Compiled and Interpreter
+ * Java File ------> Java Class --> Use
+ * a.java -- javac - Byte code -->
+ * -------- compiled------------ Interpreter
+ * 
+ * 
+ * JVM --> Interprete the byte code
+ * Byte code can be use in any platform (max,win,linux)
+ * Platform independent
+ * 
+ * Javac test.java --> compiled
+ * java test.class --> interpreted
+ * 
  * 
  * Interpreter translate one statement at a time into machine code.
  * One statement at a time
@@ -11,7 +27,7 @@ public class CompInter {
  * Easy for programmers
  * 
  * 
- * Compiler scan whole program and turn into machine code.
+ * Compiled scan whole program and turn into machine code.
  * Entire program at a time
  * Once compile
  * No execution for error
