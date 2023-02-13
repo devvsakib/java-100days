3f4da8f7324789b9fe6a0ba61fffbc8d1d768a8b
DevvSakib <devvsakib@gmail.com>
2023-02-13 23:35:51 +0600

    multithread

--- Day31/MultiThreading.java
+++ Day31/MultiThreading.java
@@ -1,31 +1,6 @@
-class MyThread extends Thread {
-    @Override
-    public void run() {
-        int i = 0;
-        while (i < 1000) {
-            System.out.println("Thread 1 Programme Runing..");
-            i++;
-        }
-    }
-}
-
-class MyThread2 extends Thread {
-    @Override
-    public void run() {
-        int i = 0;
-        while (i < 1000) {
-            System.out.println("Thread 1 Programme Runing..");
-            i++;
-        }
-    }
-}
-
 public class MultiThreading {
     public static void main(String[] args) {
-        MyThread mt = new MyThread();
-        MyThread2 mt2 = new MyThread2();
-        mt.start();
-        mt2.start();
+        System.out.println("MultiThread");
     }
 }
 /*
