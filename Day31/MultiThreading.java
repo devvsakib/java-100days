5bffade2cfb52723a333be3012a9f0f751a34d43
DevvSakib <devvsakib@gmail.com>
2023-02-13 22:59:39 +0600

    multithreading understand

---
+++ Day31/MultiThreading.java
@@ -0,0 +1,29 @@
+public class MultiThreading {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+ * Multithreading
+ * --------------
+ * Multiprocessing and multithreading both are used to achieve multitasking.
+ * 
+ * 
+ * Flow
+ * 
+ * No Threading->
+ * Main() -> func1() -> func2() -> End
+ * 
+ * Threading->
+ * Main -> Func() ===|
+ * ------------------|==> End
+ * ------- Func2() ==|
+ * 
+ * 
+ * Creating a Thread
+ * There are two ways to create a thread.
+ * i. extenda thread class
+ * ii. implementing Runnable interfaces
+ * 
+ * 
+ */
