dccc628cfb0a45c2fbae3975717accf89aa38dd1
DevvSakib <devvsakib@gmail.com>
2023-02-27 19:45:21 +0600

    Collections Framework, java.util.Collections

---
+++ Day39/CollectionFramework.java
@@ -0,0 +1,36 @@
+package com;
+
+public class CollectionFramework {
+
+}
+
+/*
+ * Collections Framework:
+ * ----------------
+ * A collection represents a group of object. Java Collections provide Classes
+ * and Intergaces for us to be able to write Code quickly and efficiently
+ * 
+ * Why Collections?
+ * ----------------
+ * We need collections for efficient storage and better manupulation of data in
+ * Java.
+ * 
+ * Ex: we use arrays to store integers but what if we want to:
+ * Resize
+ * Insert element in between
+ * Delte
+ * Apply certain operation
+ * 
+ * How are collections Available?
+ * -----------------------------
+ * They are in java.util.Collections
+ * * Class and Interfaces 
+ *
+ * ArrayList -> Variable size collection
+ * Set -> Distinct Collection
+ * Stack -> Lifo data structure(Last In, First Out)
+ * Hashmap -> Storing key-value pairs
+ * 
+ * Collection also provide
+ * 
+ */
