300be47b51839df1d324748f18b28c5c2baa87fb
DevvSakib <devvsakib@gmail.com>
2023-03-04 19:46:59 +0600

    Hashing technique

---
+++ Day41/HashMap.java
@@ -0,0 +1,25 @@
+package com;
+
+public class HashMap {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+ * Hashing Technique
+ * -----------------
+ * Hash Collision
+ * Open Addressing to handle collision
+ * chainning
+ * 
+ * Class:
+ * HashSet
+ * HashMap
+ * LinkedHashMap
+ * HashMap
+ * 
+ * 
+ */
+
+// javac -d . HashMap.java && java com/HashMap
