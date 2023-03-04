6179750e11436f72315a18bf02d1ed9c5c7ea17f
DevvSakib <devvsakib@gmail.com>
2023-03-04 19:55:07 +0600

    HashSet in Java

--- Day41/HashSetJava.java
+++ Day41/HashSetJava.java
@@ -1,5 +1,22 @@
-package Day41;
+package com;
+
+import java.util.HashSet;
 
 public class HashSetJava {
-    
+    public static void main(String[] args) {
+        HashSet<Integer> myHashSet = new HashSet<>();
+        myHashSet.add(12);
+        myHashSet.add(12);
+        myHashSet.add(15);
+        myHashSet.add(8);
+        myHashSet.add(10);
+        System.out.println(myHashSet);
+    }
 }
+
+/*
+    In Java, a HashSet is a collection class that implements the Set interface and allows you to store elements in a collection with no duplicates. It is a part of the Java Collections Framework.
+*/
+
+
+// javac -d . HashSetJava.java && java com/HashSetJava
