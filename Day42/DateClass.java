4818aa0f296c4d53abd13e294086cf333323502e
DevvSakib <devvsakib@gmail.com>
2023-03-07 20:00:28 +0600

    date class

---
+++ Day42/DateClass.java
@@ -0,0 +1,22 @@
+package com;
+
+import java.util.Date;
+
+public class DateClass{
+    public static void main(String[] args) {
+        // Date d = new Date();
+        Date d = new Date();
+        System.out.println(d.getTime());
+        // System.out.println(d.getDate()); Deprecated
+        // System.out.println(d.getYear()); Deprecated
+        // System.out.println(d.getSeconds()); Deprecated
+
+    }
+}
+
+
+/*
+    Date class
+
+    Date d = new Date()
+*/
